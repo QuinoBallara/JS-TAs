31bd0fabd92020bd0593f027d21c5009de1220be
Joaquín Ballara <joaquinballara02@gmail.com>
2024-08-21 18:47:44 -0300

    avance ta19

--- TA19/script.js
+++ TA19/script.js
@@ -1,18 +1,21 @@
 const username = document.getElementById("username");
+const userbox = document.getElementById("username-box")
 const password = document.getElementById("password");
+const passbox = document.getElementById("password-box")
 const mail = document.getElementById("mail");
+const mailbox = document.getElementById("mail-box")
 const form = document.getElementById("form");
 
 const invalidMail = document.createElement("p");
 invalidMail.textContent = "El correo es inválido."
 function validMail() {
     if (mail.value.includes("@") && mail.value.includes(".") && mail.value.split("@")[0].length > 0 && mail.value.split("@")[1].split(".")[0].length > 0 && mail.value.split("@")[1].split(".")[1].length > 0) {
-        if (invalidMail.parentElement == form) {
-            form.removeChild(invalidMail);
+        if (invalidMail.parentElement == mailbox) {
+            mailbox.removeChild(invalidMail);
             return true;
         }
     } else {
-        form.appendChild(invalidMail);
+        mailbox.appendChild(invalidMail);
         return false;
     }
 }
@@ -23,12 +26,12 @@ const invalidPass = document.createElement("p");
 invalidPass.textContent = "La contraseña debe tener más de 8 caracteres";
 function validPass() {
     if (password.value.length >= 8) {
-        if (invalidPass.parentElement == form) {
-            form.removeChild(invalidPass);
+        if (invalidPass.parentElement == passbox) {
+            passbox.removeChild(invalidPass);
             return true;
         }
     } else {
-        form.appendChild(invalidPass);
+        passbox.appendChild(invalidPass);
         return false;
     }
 }
@@ -39,14 +42,13 @@ password.addEventListener("input", validPass);
 const emptyFields = document.createElement("p");
 emptyFields.textContent = "Los campos no deben estar vacíos";
 function notEmptyFields() {
+    let i = 0
     if (username.value.length > 0 && password.value.length > 0 && mail.value.length > 0) {
-        if (emptyFields.parentElement == form) {
-            form.removeChild(emptyFields);
-            return true;
+        if (emptyFields.parentElement == userbox) {
+            userbox.removeChild(emptyFields);
         }
     } else {
-        form.appendChild(emptyFields);
-        return false;
+        userbox.appendChild(emptyFields);
     }
 }
 
