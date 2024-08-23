c0a01dc5c74fe0544a1dedf5ebba25cbe103a0d6
Joaquín Ballara <joaquinballara02@gmail.com>
2024-08-23 18:03:21 -0300

    fin

--- TA21/script.js
+++ TA21/script.js
@@ -1,7 +1,9 @@
 const input = document.getElementById("input");
 input.addEventListener("focus", function () {
-    input.style.outlineColor = "gray"
+    input.style.borderColor = "black";
+    input.style.outline = 0;
 })
 input.addEventListener("blur", function () {
-
+    input.style.borderColor = "lightgray"
 })
+input.style.borderColor = "lightgray"
